3dff0d363cf4051c68f74d85e99ba9c748726473
xpolb1 <bogdan.polovko@gmail.com>
2016-11-28 10:52:16 -0500

    Initialized the project

---
+++ public/js/options.js
@@ -0,0 +1,42 @@
+'use strict';
+/* global $ tripModule attractionsModule hotels restaurants activities */
+
+/**
+ * This module fills the `select` tags with `option`s.
+ * It runs immediately upon document ready (not called by other modules).
+ * Each `option` displays the name of an attraction and is has a value of
+ * that attraction's id. Selecting an option looks up the attraction by id,
+ * then tells the trip module to add the attraction.
+ */
+
+$(function(){
+
+  // jQuery selects
+  var $optionsPanel = $('#options-panel');
+  var $hotelSelect = $optionsPanel.find('#hotel-choices');
+  var $restaurantSelect = $optionsPanel.find('#restaurant-choices');
+  var $activitySelect = $optionsPanel.find('#activity-choices');
+
+  // make all the option tags (second arg of `forEach` is a `this` binding)
+  hotels.forEach(makeOption, $hotelSelect);
+  restaurants.forEach(makeOption, $restaurantSelect);
+  activities.forEach(makeOption, $activitySelect);
+
+  function makeOption (databaseAttraction) {
+    var $option = $('<option></option>') // makes a new option tag
+      .text(databaseAttraction.name)
+      .val(databaseAttraction.id);
+    this.append($option); // add the option to the specific select
+  }
+
+  // what to do when the `+` button next to a `select` is clicked
+  $optionsPanel.on('click', 'button[data-action="add"]', function () {
+    var $select = $(this).siblings('select');
+    var type = $select.data('type'); // from HTML data-type attribute
+    var id = $select.find(':selected').val();
+    // get associated attraction and add it to the current day in the trip
+    var attraction = attractionsModule.getByTypeAndId(type, id);
+    tripModule.addToCurrent(attraction);
+  });
+
+});
---
+++ public/js/trip.js
@@ -0,0 +1,95 @@
+'use strict';
+/* global $ dayModule */
+
+/**
+ * A module for managing multiple days & application state.
+ * Days are held in a `days` array, with a reference to the `currentDay`.
+ * Clicking the "add" (+) button builds a new day object (see `day.js`)
+ * and switches to displaying it. Clicking the "remove" button (x) performs
+ * the relatively involved logic of reassigning all day numbers and splicing
+ * the day out of the collection.
+ *
+ * This module has four public methods: `.load()`, which currently just
+ * adds a single day (assuming a priori no days); `switchTo`, which manages
+ * hiding and showing the proper days; and `addToCurrent`/`removeFromCurrent`,
+ * which take `attraction` objects and pass them to `currentDay`.
+ */
+
+var tripModule = (function () {
+
+  // application state
+
+  var days = [],
+      currentDay;
+
+  // jQuery selections
+
+  var $addButton, $removeButton;
+  $(function () {
+    $addButton = $('#day-add');
+    $removeButton = $('#day-title > button.remove');
+  });
+
+  // method used both internally and externally
+
+  function switchTo (newCurrentDay) {
+    if (currentDay) currentDay.hide();
+    currentDay = newCurrentDay;
+    currentDay.show();
+  }
+
+  // jQuery event binding
+
+  $(function () {
+    $addButton.on('click', addDay);
+    $removeButton.on('click', deleteCurrentDay);
+  });
+
+  function addDay () {
+    if (this && this.blur) this.blur(); // removes focus box from buttons
+    var newDay = dayModule.create({ number: days.length + 1 }); // dayModule
+    days.push(newDay);
+    if (days.length === 1) {
+      currentDay = newDay;
+    }
+    switchTo(newDay);
+  }
+
+  function deleteCurrentDay () {
+    // prevent deleting last day
+    if (days.length < 2 || !currentDay) return;
+    // remove from the collection
+    var index = days.indexOf(currentDay),
+      previousDay = days.splice(index, 1)[0],
+      newCurrent = days[index] || days[index - 1];
+    // fix the remaining day numbers
+    days.forEach(function (day, i) {
+      day.setNumber(i + 1);
+    });
+    switchTo(newCurrent);
+    previousDay.hideButton();
+  }
+
+  // globally accessible module methods
+
+  var publicAPI = {
+
+    load: function () {
+      $(addDay);
+    },
+
+    switchTo: switchTo,
+
+    addToCurrent: function (attraction) {
+      currentDay.addAttraction(attraction);
+    },
+
+    removeFromCurrent: function (attraction) {
+      currentDay.removeAttraction(attraction);
+    }
+
+  };
+
+  return publicAPI;
+
+}());
